e99486635a21e6425472be7f367516d65f48f664
Hxx <uuuu43029@gmail.com>
2026-07-09 18:01:40 +0800

    feat(claude): enable category card layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // import '@/styles/animate.css' // @see https://animate.style/
 import '@/styles/globals.css'
 import '@/styles/utility-patterns.css'
+import '@/styles/claude-category-cards.css'
 
 // core styles shared by all of react-notion-x (required)
 import '@/styles/notion.css' //  重写部分notion样式

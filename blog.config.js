ca7a8b4a3bf62b9039b18a9a6b9e1a15e45ee495
Hxx <uuuu43029@gmail.com>
2026-08-04 23:31:36 +0900

    chore: keep unrelated configuration comments unchanged

--- blog.config.js
+++ blog.config.js
@@ -47,7 +47,7 @@ const BLOG = {
   ...require('./conf/analytics.config'), // 站点访问统计
   ...require('./conf/image.config'), // 网站图片相关配置
   ...require('./conf/font.config'), // 网站字体
-  ...require('./conf/right-click-menu'), // 自定义右键菜单相关设置
+  ...require('./conf/right-click-menu'), // 自定义右键菜单相关配置
   ...require('./conf/code.config'), // 网站代码块样式
   ...require('./conf/animation.config'), // 动效美化效果
   ...require('./conf/widget.config'), // 悬浮在网页上的挂件，聊天客服、宠物挂件、音乐播放器等

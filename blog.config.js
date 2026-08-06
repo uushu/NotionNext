6c0293fa7d578d5ecd60c1161dc8a29286468bfd
Hxx <uuuu43029@gmail.com>
2026-08-07 01:51:22 +0900

    chore: restore unrelated config comment

--- blog.config.js
+++ blog.config.js
@@ -66,7 +66,7 @@ const BLOG = {
 
   // 自定义外部脚本，外部样式
   CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
-  CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/script.js']
+  CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 自定义菜单
   CUSTOM_MENU: parseBooleanEnv(process.env.NEXT_PUBLIC_CUSTOM_MENU, true), // 支持Menu类型的菜单，替代了3.12版本前的Page类型
